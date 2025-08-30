496df8a6acf27f705a832e2028bd56594ad26b6d
Prakhar Prakash <prakharprakash159@gmail.com>
2025-08-30 23:51:39 +0530

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,7 +9,7 @@ import Login from './pages/Login'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
--- Frontend/src/pages/MyAppointments.jsx
+++ Frontend/src/pages/MyAppointments.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { AppContext } from '../context/Appcontext';
+import { AppContext } from '../context/AppContext';
 import { toast } from 'react-toastify';
 import {useNavigate } from 'react-router-dom';
 
